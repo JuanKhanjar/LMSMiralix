6ac4ab33a627d1ba4f4eeeb01ca16903823b7e1f
Juan Khanjar <141147237+JuanKhanjarM@users.noreply.github.com>
2023-09-14 14:27:39 +0200

    makes some refine

---
+++ LMS.BlazorApp/Helpers/SweetAlertService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.JSInterop;
+
+namespace LMS.BlazorApp.Helpers
+{
+    public class SweetAlertService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public SweetAlertService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task<bool> ShowConfirmationAsync(string message)
+        {
+            return await _jsRuntime.InvokeAsync<bool>("showSweetAlertConfirmation", message);
+        }
+
+        public async Task ShowSuccessAsync(string title, string message)
+        {
+            await _jsRuntime.InvokeVoidAsync("showSweetAlertSuccess", title, message);
+        }
+
+        public async Task ShowValidationErrorAsync(string message)
+        {
+            await _jsRuntime.InvokeVoidAsync("showValidationError", message);
+        }
+    }
+
+}
