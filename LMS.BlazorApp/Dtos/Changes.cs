d77d406746e50de6c4ea9480c3f7d1ba03c70470
Juan Khanjar <141147237+JuanKhanjarM@users.noreply.github.com>
2023-09-21 13:43:47 +0200

    Dialog work

---
+++ LMS.BlazorApp/Dtos/Changes.cs
@@ -0,0 +1,13 @@
+﻿namespace LMS.BlazorApp.Dtos
+{
+    public class Changes
+    {
+        public int CustomerId { get; set; }
+        public int GroupId { get; set; }
+        public int LicenseId { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public int GPChangeQuantity { get; set; }
+        public int PPChangeQuantity { get; set; }
+    }
+}
