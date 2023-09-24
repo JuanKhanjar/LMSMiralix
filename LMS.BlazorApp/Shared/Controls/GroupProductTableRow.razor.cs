9d7c7541a12c32094e1066576aacd764caf9227b
juankhanjar <93974472+juankhanjar@users.noreply.github.com>
2023-09-24 14:41:19 +0200

    ddd

---
+++ LMS.BlazorApp/Shared/Controls/GroupProductTableRow.razor.cs
@@ -0,0 +1,19 @@
+﻿using LMS.BlazorApp.Dtos;
+using LMS.BusinessCore.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace LMS.BlazorApp.Shared.Controls
+{
+    public partial class GroupProductTableRow
+    {
+
+        [Parameter]
+        public GroupProductDto GroupProduct { get; set; } = new GroupProductDto();
+
+        [Parameter]
+        public int PurchasedProduct { get; set; }
+        [Parameter]
+        public EventCallback<GroupProductDto> RemoveProduct { get; set; }
+
+    }
+}
---
+++ LMS.BlazorApp/Shared/Controls/PurchasedProductTableRow.razor.cs
@@ -0,0 +1,14 @@
+﻿using LMS.BlazorApp.Dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace LMS.BlazorApp.Shared.Controls
+{
+    public partial class PurchasedProductTableRow
+    {
+        [Parameter]
+        public PurchasedProductDto PurchasedProduct { get; set; } = new PurchasedProductDto();
+        [Parameter]
+        public int GroupProductQuantity { get; set; }
+
+    }
+}
