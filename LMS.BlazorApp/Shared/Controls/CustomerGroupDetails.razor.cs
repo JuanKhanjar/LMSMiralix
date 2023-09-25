a046760ebcd2004ace8434b2f17ae750b63afca6
juankhanjar <93974472+juankhanjar@users.noreply.github.com>
2023-09-25 20:37:22 +0200

    ggg

---
+++ LMS.BlazorApp/Shared/Controls/CustomerGroupDetails.razor.cs
@@ -0,0 +1,18 @@
+﻿using LMS.BusinessCore.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace LMS.BlazorApp.Shared.Controls
+{
+    public partial class CustomerGroupDetails
+    {
+        [Parameter]
+        public Customer _customer { get; set; } = new Customer();
+
+        [Parameter]
+        public Group group { get; set; } = new Group();
+        [Parameter]
+        public EventCallback<int> ShowGroupDetails { get; set; }
+        [Parameter]
+        public EventCallback<int> DeleteGroupWithAllItsGP { get; set; }
+    }
+}
