0b6a6468d818732226d80ab59b07671ec31e164b
Juan Khanjar <141147237+JuanKhanjarM@users.noreply.github.com>
2023-10-16 08:12:05 +0200

    Juan

---
+++ Plugins/LMS.SqlServer/Migrations/20231016052847_Emailsfields.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Emailsfields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 1,
+                column: "RegistrationDate",
+                value: new DateTime(2023, 10, 16, 7, 28, 47, 695, DateTimeKind.Local).AddTicks(671));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 2,
+                column: "RegistrationDate",
+                value: new DateTime(2023, 10, 16, 7, 28, 47, 695, DateTimeKind.Local).AddTicks(719));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 1,
+                column: "RegistrationDate",
+                value: new DateTime(2023, 10, 13, 18, 39, 47, 71, DateTimeKind.Local).AddTicks(6392));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 2,
+                column: "RegistrationDate",
+                value: new DateTime(2023, 10, 13, 18, 39, 47, 71, DateTimeKind.Local).AddTicks(6438));
+        }
+    }
+}
