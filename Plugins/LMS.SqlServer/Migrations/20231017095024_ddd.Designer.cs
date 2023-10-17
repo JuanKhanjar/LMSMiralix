68f56ea6bff5c9a0b71cf57f694308a5f5b3294a
Juan Khanjar <141147237+JuanKhanjarM@users.noreply.github.com>
2023-10-17 11:52:32 +0200

    Secuer Database

--- Plugins/LMS.SqlServer/Migrations/20231017095024_ddd.Designer.cs
+++ Plugins/LMS.SqlServer/Migrations/20231017095024_ddd.Designer.cs
@@ -4,6 +4,7 @@ using LMS.SqlServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LMS.SqlServer.Migrations
 {
     [DbContext(typeof(LMSDbContext))]
-    partial class LMSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231017095024_ddd")]
+    partial class ddd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Plugins/LMS.SqlServer/Migrations/20231017095024_ddd.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ddd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 1,
+                column: "RegistrationDate",
+                value: new DateTime(2023, 10, 17, 11, 50, 24, 829, DateTimeKind.Local).AddTicks(820));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 2,
+                column: "RegistrationDate",
+                value: new DateTime(2023, 10, 17, 11, 50, 24, 829, DateTimeKind.Local).AddTicks(863));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 1,
+                column: "RegistrationDate",
+                value: new DateTime(2023, 10, 16, 7, 28, 47, 695, DateTimeKind.Local).AddTicks(671));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 2,
+                column: "RegistrationDate",
+                value: new DateTime(2023, 10, 16, 7, 28, 47, 695, DateTimeKind.Local).AddTicks(719));
+        }
+    }
+}
