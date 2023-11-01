53b03abde5dcf489db0f3f2294d90d817cbb0ed1
Juan Khanjar <141147237+JuanKhanjarM@users.noreply.github.com>
2023-11-01 12:51:21 +0100

    login

---
+++ LMS.BlazorApp/Data/Migrations/20231101103853_logIn.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.BlazorApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class logIn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
